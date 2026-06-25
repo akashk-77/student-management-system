1c69a8e4f5a70d03182ee70cd8b26bafd01df65f
akashk-77 <akashkxy@gmail.com>
2026-06-25 17:01:48 +0530

    Update database URL with new password

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "mysql+pymysql://root:Goose22%402008.in@localhost/studentdb"
+DATABASE_URL = "mysql+pymysql://root:password@localhost/studentdb"
 
 engine = create_engine(DATABASE_URL)
