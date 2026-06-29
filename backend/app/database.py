1d3c704dbd699025c1735070eb302343ec1d0aad
akashk-77 <akashkxy@gmail.com>
2026-06-29 22:26:03 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,9 @@
+import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = "mysql+pymysql://root:password@localhost/studentdb"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:root@localhost/student_management"
+)
 
 engine = create_engine(DATABASE_URL)
